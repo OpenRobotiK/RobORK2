a9ccf57685051f3428fc38d4920d5eca46dbdfde
Mael GAUDY <mael.gaudy@gmail.com>
2012-11-05 21:59:34 +0100

    Programmation, Ajout du contrôle des broches pour déterminer le sens de rotation.

--- Programmation/PWM_test/src/motors.c
+++ Programmation/PWM_test/src/motors.c
@@ -14,10 +14,23 @@ void MOTOR_Init(uint32_t motor) {
 
 		PWM_Init(PWM1, 50);
 
+		LPC_PINCON->PINSEL4 &= 0xFFFC0FFF; //  P2.6/7/8 are configured as GPIO.
+		LPC_GPIO2->FIODIR0 |= (1<<6) || (1<<7) ; // P2.6/7 are configured as output.
+		LPC_GPIO2->FIODIR1 |= (1<<0); // P2.8 is configured as output.
+		LPC_GPIO2->FIOSET0 |= (1<<6) ; // P2.6 is set to 1.
+		LPC_GPIO2->FIOSET0 &= (0<<7 ); //P2.7 is set to 0.
+		LPC_GPIO2->FIOSET1 |= (1<< 0); //P2.8 is set to 1.
+
 
 	} else if(motor == MOTOR_LEFT) {
 
 		PWM_Init(PWM2, 50);
 
+		LPC_PINCON->PINSEL4 &= 0xFC0FFFFF;
+
+		LPC_GPIO2->FIODIR1 |= (1<<2) || (1<<3) || (1<<4); // P2.10/11/12 are configured as output.
+		LPC_GPIO2->FIOSET1 |= (1<<2); // P2.10 is set to 1.
+		LPC_GPIO2->FIOSET1 &= (0<<3 ); //P2.11 is set to 0.
+		LPC_GPIO2->FIOSET1 |= (1<< 4); //P2.12 is set to 1.
 	}
 }
