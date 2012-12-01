7617f627f35174459af94b22e89a67fbb3c336ab
Lyphout Florent <lyphout.florent@gmail.com>
2012-12-01 17:56:09 +0100

    interruption codeur
    j'ai testé que l'interruption n°3

--- Programmation/RobORK/src/drivers/codeur.h
+++ Programmation/RobORK/src/drivers/codeur.h
@@ -1,13 +1,48 @@
 #ifndef CODEUR_H_
 #define CODEUR_H_
 
+#include "LPC17xx.h"
 #include "defines.h"
+#include "uart.h"
+
 
 typedef enum codeur codeur;
 enum codeur
 { codeur_droit, codeur_gauche};
 
-void init_interrupt_codeur(codeur nom_codeur);
-void EINT0_IRQHandler(void);
+#define EINT0			0x00000001
+#define EINT1			0x00000002
+#define EINT2			0x00000004
+#define EINT3			0x00000008
+
+#define EINT0_EDGE		0x00000001
+#define EINT1_EDGE		0x00000002
+#define EINT2_EDGE		0x00000004
+#define EINT3_EDGE		0x00000008
+
+#define EINT0_RISING	0x00000001
+#define EINT1_RISING	0x00000002
+#define EINT2_RISING	0x00000004
+#define EINT3_RISING	0x00000008
+
+#define SELECT_EINT0	0x00100000
+#define SELECT_EINT1	0x00400000
+#define SELECT_EINT2	0x01000000
+#define SELECT_EINT3	0x04000000
+
+#define CLEAR_EINT0		0x00000400
+#define CLEAR_EINT1		0x00000800
+#define CLEAR_EINT2		0x00001000
+#define CLEAR_EINT3		0x00002000
+
+extern void EINT3_IRQHandler(void);
+extern bool EINT3Init( void );
+extern void EINT2_IRQHandler(void);
+extern bool EINT2Init( void );
+extern void EINT1_IRQHandler(void);
+extern bool EINT1Init( void );
+extern void EINT0_IRQHandler(void);
+extern bool EINT0Init( void );
+extern bool init_interrupt_codeur(void);
 
 #endif /* CODEUR_H_ */
