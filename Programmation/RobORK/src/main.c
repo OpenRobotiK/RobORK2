1909fe210f8f5c63939c42f91aeb0063f12936ec
Lyphout Florent <lyphout.florent@gmail.com>
2012-12-06 23:25:34 +0100

    ajout du jack mais j'ai un pb avec l'init du jack

--- Programmation/RobORK/src/main.c
+++ Programmation/RobORK/src/main.c
@@ -21,7 +21,7 @@ Description : main definition
 #include "uart.h"
 #include "timer.h"
 #include "codeur.h"
-
+#include "autre.h"
 
 
 int main(void)
@@ -42,6 +42,7 @@ int main(void)
 
 
 
+	init_jack();
 
 
 	while(1)
@@ -53,205 +54,210 @@ int main(void)
 			send_message(buf);
 		}//*/
 
-		/******************************************************/
-		/************** debut du mode "demo" ******************/
-		/******************************************************/
-
-		while(demo_mode==true)
+			/******************************************************/
+			/************** debut du mode "demo" ******************/
+			/******************************************************/
+		while (attendre_jack()==false)
 		{
-			if (asserv==true)
+			while(demo_mode==true)
 			{
-				int_to_char(nombre_a_regarder,buf);
-				send_message(buf);
-				asserv=false;
-			}
-			if (avance==true)
-			{
-				LPC_GPIO0->FIOSET |= (1<<22);//|= (1<<7); //p0.22 INa
-				LPC_GPIO0->FIOCLR |=(1<<21);//|= (1<<4); //p0.27 INb
-				LPC_GPIO0->FIOSET |= (1<<3); //p0.28 DIAG
-
-				LPC_GPIO2->FIOCLR0 |= (1<<6) ; // P2.6 is set to 1. InA
-				LPC_GPIO2->FIOSET0 |= (1<<7 ); //P2.7 is set to 0. InB
-				LPC_GPIO2->FIOSET1 |= (1<< 0); //P2.8 is set to 1. Diag
-
-				avance=false;
-			}
-			else if (recule==true)
-			{
-				LPC_GPIO0->FIOCLR |= (1<<22);//|= (1<<7); //p0.22 INa
-				LPC_GPIO0->FIOSET |=(1<<21);//|= (1<<4); //p0.27 INb
-				LPC_GPIO0->FIOSET |= (1<<3); //p0.28 DIAG
-
-				LPC_GPIO2->FIOSET0 |= (1<<6) ; // P2.6 is set to 1. InA
-				LPC_GPIO2->FIOCLR0 |= (1<<7 ); //P2.7 is set to 0. InB
-				LPC_GPIO2->FIOSET1 |= (1<< 0); //P2.8 is set to 1. Diag
-
-				recule=false;
-			}
-			else if (gauche==true)
-			{
-				LPC_GPIO0->FIOSET |= (1<<22);//|= (1<<7); //p0.22 INa
-				LPC_GPIO0->FIOCLR |=(1<<21);//|= (1<<4); //p0.27 INb
-				LPC_GPIO0->FIOSET |= (1<<3); //p0.28 DIAG
-
-				LPC_GPIO2->FIOSET0 |= (1<<6) ; // P2.6 is set to 1. InA
-				LPC_GPIO2->FIOCLR0 |= (1<<7 ); //P2.7 is set to 0. InB
-				LPC_GPIO2->FIOSET1 |= (1<< 0); //P2.8 is set to 1. Diag
-
-				gauche=false;
-			}
-			else if (droite==true)
-			{
-				LPC_GPIO0->FIOCLR |= (1<<22);//|= (1<<7); //p0.22 INa
-				LPC_GPIO0->FIOSET |=(1<<21);//|= (1<<4); //p0.27 INb
-				LPC_GPIO0->FIOSET |= (1<<3); //p0.28 DIAG
-
-				LPC_GPIO2->FIOCLR0 |= (1<<6) ; // P2.6 is set to 1. InA
-				LPC_GPIO2->FIOSET0 |= (1<<7 ); //P2.7 is set to 0. InB
-				LPC_GPIO2->FIOSET1 |= (1<< 0); //P2.8 is set to 1. Diag
-
-				droite=false;
-			}
-			else if (stop==true)
-			{
-				/* stop les moteur au demarage*/
-				LPC_GPIO2->FIOCLR1 |= (1<< 0); //P2.8 is set to 1. Diag
-
-				LPC_GPIO0->FIOCLR |= (1<<3); //p0.28 DIAG
-				//LPC_GPIO0->FIOCLR3 |= (1<<5); //p0.28 DIAG
-
-				erreur_precedente_gauche = 0;
-				somme_erreur_gauche = 0;
-				erreur_precedente_droit = 0;
-				somme_erreur_droit = 0;
-				stop=false;
-			}
-			else if (regle_vitesse==true)
-			{
-				validation = false;
-				send_message("\n\r\n\r");
-				send_message("tape la vistesse en %\n\r");
-				for (i=0;i==strlen(commande);i++)
+				if (asserv==true)
 				{
-					commande[i]='\0';
+					int_to_char(nombre_a_regarder,buf);
+					send_message(buf);
+					asserv=false;
 				}
-				while (validation==false){}
-				consigne_moteur_nombre_tours_par_seconde_gauche=atoi(commande);
-				//PWM_SetDutyCycle(PWM2,atoi(commande)); // refaire la fonction atoi pour virer stdlib.h
-				//PWM_SetDutyCycle(PWM1,atoi(commande));
-				send_message(commande);
-				send_message("\n\r\n\r");
-				for (i=0;i==strlen(commande);i++)
+				if (avance==true)
 				{
-					commande[i]='\0';
+					LPC_GPIO0->FIOSET |= (1<<22);//|= (1<<7); //p0.22 INa
+					LPC_GPIO0->FIOCLR |=(1<<21);//|= (1<<4); //p0.27 INb
+					LPC_GPIO0->FIOSET |= (1<<3); //p0.28 DIAG
+
+					LPC_GPIO2->FIOCLR0 |= (1<<6) ; // P2.6 is set to 1. InA
+					LPC_GPIO2->FIOSET0 |= (1<<7 ); //P2.7 is set to 0. InB
+					LPC_GPIO2->FIOSET1 |= (1<< 0); //P2.8 is set to 1. Diag
+
+					avance=false;
 				}
-				erreur_precedente_gauche = 0;
-				somme_erreur_gauche = 0;
-				erreur_precedente_droit = 0;
-				somme_erreur_droit = 0;
-				regle_vitesse=false;
-		}
+				else if (recule==true)
+				{
+					LPC_GPIO0->FIOCLR |= (1<<22);//|= (1<<7); //p0.22 INa
+					LPC_GPIO0->FIOSET |=(1<<21);//|= (1<<4); //p0.27 INb
+					LPC_GPIO0->FIOSET |= (1<<3); //p0.28 DIAG
+
+					LPC_GPIO2->FIOSET0 |= (1<<6) ; // P2.6 is set to 1. InA
+					LPC_GPIO2->FIOCLR0 |= (1<<7 ); //P2.7 is set to 0. InB
+					LPC_GPIO2->FIOSET1 |= (1<< 0); //P2.8 is set to 1. Diag
+
+					recule=false;
+				}
+				else if (gauche==true)
+				{
+					LPC_GPIO0->FIOSET |= (1<<22);//|= (1<<7); //p0.22 INa
+					LPC_GPIO0->FIOCLR |=(1<<21);//|= (1<<4); //p0.27 INb
+					LPC_GPIO0->FIOSET |= (1<<3); //p0.28 DIAG
+
+					LPC_GPIO2->FIOSET0 |= (1<<6) ; // P2.6 is set to 1. InA
+					LPC_GPIO2->FIOCLR0 |= (1<<7 ); //P2.7 is set to 0. InB
+					LPC_GPIO2->FIOSET1 |= (1<< 0); //P2.8 is set to 1. Diag
+
+					gauche=false;
+				}
+				else if (droite==true)
+				{
+					LPC_GPIO0->FIOCLR |= (1<<22);//|= (1<<7); //p0.22 INa
+					LPC_GPIO0->FIOSET |=(1<<21);//|= (1<<4); //p0.27 INb
+					LPC_GPIO0->FIOSET |= (1<<3); //p0.28 DIAG
+
+					LPC_GPIO2->FIOCLR0 |= (1<<6) ; // P2.6 is set to 1. InA
+					LPC_GPIO2->FIOSET0 |= (1<<7 ); //P2.7 is set to 0. InB
+					LPC_GPIO2->FIOSET1 |= (1<< 0); //P2.8 is set to 1. Diag
+
+					droite=false;
+				}
+				else if (stop==true)
+				{
+					/* stop les moteur au demarage*/
+					LPC_GPIO2->FIOCLR1 |= (1<< 0); //P2.8 is set to 1. Diag
+
+					LPC_GPIO0->FIOCLR |= (1<<3); //p0.28 DIAG
+					//LPC_GPIO0->FIOCLR3 |= (1<<5); //p0.28 DIAG
+
+					erreur_precedente_gauche = 0;
+					somme_erreur_gauche = 0;
+					erreur_precedente_droit = 0;
+					somme_erreur_droit = 0;
+					stop=false;
+				}
+				else if (regle_vitesse==true)
+				{
+					validation = false;
+					send_message("\n\r\n\r");
+					send_message("tape la vistesse en %\n\r");
+					for (i=0;i==strlen(commande);i++)
+					{
+						commande[i]='\0';
+					}
+					while (validation==false){}
+					consigne_moteur_nombre_tours_par_seconde_gauche=atoi(commande);
+					//PWM_SetDutyCycle(PWM2,atoi(commande)); // refaire la fonction atoi pour virer stdlib.h
+					//PWM_SetDutyCycle(PWM1,atoi(commande));
+					send_message(commande);
+					send_message("\n\r\n\r");
+					for (i=0;i==strlen(commande);i++)
+					{
+						commande[i]='\0';
+					}
+					erreur_precedente_gauche = 0;
+					somme_erreur_gauche = 0;
+					erreur_precedente_droit = 0;
+					somme_erreur_droit = 0;
+					regle_vitesse=false;
+
+			}
+				if (validation==true)
+			{
+				if(strcmp(commande,"exit")==0 )
+				{
+					demo_mode=false;
+					send_message("\n\rtu es sortie du mode demo\n\r");
+				}
+			}
+			}
+
+
+			/******************************************************/
+			/**************** fin du mode "demo" ******************/
+			/******************************************************/
+
+
+			/******************************************************/
+			/************* debut de la gestion uart ***************/
+			/******************************************************/
+
+
+
 			if (validation==true)
+			{
+				if(strcmp(commande,"test")==0 )
+				{
+					send_message("\n\rOK c'est bon\n\r");
+					validation = false;
+				}
+				else if(strcmp(commande,"demo")==0 )
+				{
+					demo_mode=true;
+					send_message("\n\rtu es dans le mode demo\n\rutilise :\n\r\t"
+					"z pour avancer\n\r\t"
+					"s pour reculer\n\r\t"
+					"q pour tourner a gauche\n\r\t"
+					"d pour tourner a droite\n\r\t"
+					"a pour stoper\n\r\t"
+					"v pour regler la vitesse\n\r");
+					validation = false;
+				}
+				else
+				{
+					send_message("\n\rRien du tout\n\r");
+					validation = false;
+				}
+
+			/*fin test adc*/
+			for (i=0;i==strlen(commande);i++)
+			{
+				commande[i]='\0';
+			}
+
+
+			}
+			/******************************************************/
+			/*************** fin de la gestion uart ***************/
+			/******************************************************/
+
+
+
+			/******************************************************/
+			/**************** debut de la strategie ***************/
+		/******************************************************/
+		}
+		while (1)
 		{
-			if(strcmp(commande,"exit")==0 )
+			//send_message("ok c'est bon\n\r");
+			if (timer_active==true)
 			{
-				demo_mode=false;
-				send_message("\n\rtu es sortie du mode demo\n\r");
+				timer_active=false;
+				if (timer0==30)
+				{
+					erreur_precedente_gauche = 0;
+					somme_erreur_gauche = 0;
+					erreur_precedente_droit = 0;
+					somme_erreur_droit = 0;
+					consigne_moteur_nombre_tours_par_seconde_gauche = 3;
+					consigne_moteur_nombre_tours_par_seconde_droit = 3;
+					LPC_GPIO0->FIOSET |= (1<<22);//|= (1<<7); //p0.22 INa
+					LPC_GPIO0->FIOCLR |=(1<<21);//|= (1<<4); //p0.27 INb
+					LPC_GPIO0->FIOSET |= (1<<3); //p0.28 DIAG
+
+					LPC_GPIO2->FIOCLR0 |= (1<<6) ; // P2.6 is set to 1. InA
+					LPC_GPIO2->FIOSET0 |= (1<<7 ); //P2.7 is set to 0. InB
+					LPC_GPIO2->FIOSET1 |= (1<< 0); //P2.8 is set to 1. Diag
+
+				}
+				else if (timer0==90*1000)
+				{
+					LPC_GPIO2->FIOCLR1 |= (1<< 0); //P2.8 is set to 1. Diag
+
+					LPC_GPIO0->FIOCLR |= (1<<3); //p0.28 DIAG
+					//LPC_GPIO0->FIOCLR3 |= (1<<5); //p0.28 DIAG
+
+					erreur_precedente_gauche = 0;
+					somme_erreur_gauche = 0;
+					erreur_precedente_droit = 0;
+					somme_erreur_droit = 0;
+				}
 			}
 		}
-		}
 
-
-		/******************************************************/
-		/**************** fin du mode "demo" ******************/
-		/******************************************************/
-
-
-
-
-		/******************************************************/
-		/************* debut de la gestion uart ***************/
-		/******************************************************/
-
-
-
-		if (validation==true)
-		{
-			if(strcmp(commande,"test")==0 )
-			{
-				send_message("\n\rOK c'est bon\n\r");
-				validation = false;
-			}
-			else if(strcmp(commande,"demo")==0 )
-			{
-				demo_mode=true;
-				send_message("\n\rtu es dans le mode demo\n\rutilise :\n\r\t"
-				"z pour avancer\n\r\t"
-				"s pour reculer\n\r\t"
-				"q pour tourner a gauche\n\r\t"
-				"d pour tourner a droite\n\r\t"
-				"a pour stoper\n\r\t"
-				"v pour regler la vitesse\n\r");
-				validation = false;
-			}
-			else
-			{
-				send_message("\n\rRien du tout\n\r");
-				validation = false;
-			}
-
-		/*fin test adc*/
-		for (i=0;i==strlen(commande);i++)
-		{
-			commande[i]='\0';
-		}
-		}
-
-
-		/******************************************************/
-		/*************** fin de la gestion uart ***************/
-		/******************************************************/
-
-
-
-		/******************************************************/
-		/**************** debut de la strategie ***************/
-		/******************************************************/
-
-		if (timer_active==true)
-		{
-			timer_active=false;
-			if (timer0==3000)
-			{
-				erreur_precedente_gauche = 0;
-				somme_erreur_gauche = 0;
-				erreur_precedente_droit = 0;
-				somme_erreur_droit = 0;
-				consigne_moteur_nombre_tours_par_seconde_gauche = 3;
-				consigne_moteur_nombre_tours_par_seconde_droit = 3;
-				LPC_GPIO0->FIOSET |= (1<<22);//|= (1<<7); //p0.22 INa
-				LPC_GPIO0->FIOCLR |=(1<<21);//|= (1<<4); //p0.27 INb
-				LPC_GPIO0->FIOSET |= (1<<3); //p0.28 DIAG
-
-				LPC_GPIO2->FIOCLR0 |= (1<<6) ; // P2.6 is set to 1. InA
-				LPC_GPIO2->FIOSET0 |= (1<<7 ); //P2.7 is set to 0. InB
-				LPC_GPIO2->FIOSET1 |= (1<< 0); //P2.8 is set to 1. Diag
-
-			}
-			else if (timer0==90*1000)
-			{
-				LPC_GPIO2->FIOCLR1 |= (1<< 0); //P2.8 is set to 1. Diag
-
-				LPC_GPIO0->FIOCLR |= (1<<3); //p0.28 DIAG
-				//LPC_GPIO0->FIOCLR3 |= (1<<5); //p0.28 DIAG
-
-				erreur_precedente_gauche = 0;
-				somme_erreur_gauche = 0;
-				erreur_precedente_droit = 0;
-				somme_erreur_droit = 0;
-			}
-		}
 
 		/******************************************************/
 		/**************** fin de la strategie ***************/
